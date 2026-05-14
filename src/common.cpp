772a9feb5263a9516f6569725ab117fbe2c1bcaf
MasazumiImai <imai.masazumi.p2@dc.tohoku.ac.jp>
2026-05-15 02:43:51 +0900

    Rename traj_gen to common

--- src/common.cpp
+++ src/common.cpp
@@ -12,11 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "traj_gen/traj_gen.hpp"
-
-#include <iostream>
-
-#define DEBUG false
+#include "traj_gen/common.hpp"
 
 namespace traj_gen
 {
